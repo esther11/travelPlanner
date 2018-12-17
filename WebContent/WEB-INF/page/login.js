953ad6df576fea61ac110f0fa547c24b5148dec4
Peiyu Yi <peiyuyi@D-10-19-7-213.dhcp4.washington.edu>
2018-12-16 17:16:23 -0800

    second commit

---
+++ WebContent/WEB-INF/page/login.js
@@ -0,0 +1,71 @@
+(function() {
+  "use strict";
+
+  window.addEventListener("load", initialize);
+
+  function initialize() {
+    $("login-btn").addEventListener("click", login);
+  }
+
+  function login() {
+    let email = $("email").value;
+    let password = $("password").value;
+
+    let url = "./login";
+    let obj = {user_id: email, password: password};
+    let req = JSON.stringify(obj);
+
+    ajax("POST", url, req, successLogin, showLoginError);
+  }
+
+  function successLogin(response) {
+    var parsedResponse = JSON.parse(response);
+    if (parsedResponse.result === "SUCCESS") {
+      // the returned response should be something like this:
+      // { result: "SUCCESS", ...}
+      // what the page should be like when user successfully login in?
+    }
+  }
+
+  function showLoginError() {
+    $("login-error").innerText = "Invalid email or password";
+  }
+
+  /**
+  * return the dom node of the given id
+  * @param {string} id - id of the element
+  * @return {node} - dom node of the given id
+  */
+  function $(id) {
+    return document.getElementById(id);
+  }
+
+  function ajax(method, url, data, callback, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+				callback(xhr.responseText);
+			} else if (xhr.status === 403) {
+				onSessionInvalid();
+			} else {
+				errorHandler();
+			}
+    }
+
+    xhr.onerror = function() {
+      console.error("The request couldn't be completed.");
+			errorHandler();
+    }
+
+    if (data === null) {
+			xhr.send();
+		} else {
+			xhr.setRequestHeader("Content-Type",
+					"application/json;charset=utf-8");
+			xhr.send(data);
+		}
+  }
+
+})();
