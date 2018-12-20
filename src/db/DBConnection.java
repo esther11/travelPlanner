83ef9a41f5825025b7106d978a345749f570a048
unknown <georland@umich.edu>
2018-12-20 03:21:43 -0800

    Some order updating functions have been implemented

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -1,5 +1,132 @@
 package db;
 
+import java.util.List;
+import java.util.Set;
+
+import entity.Place;
+
 public interface DBConnection {
+	/**
+	 * Close the connection.
+	 */
+	public void close();
+
+	/**
+	 * Insert the favorite places for a user.
+	 * 
+	 * @param userId
+	 * @param placeIds
+	 */
+	public void setFavoritePlaces(String userId, List<String> placeIds);
+
+	/**
+	 * Delete the favorite places for a user.
+	 * 
+	 * @param userId
+	 * @param placeIds
+	 */
+	public void unsetFavoritePlaces(String userId, List<String> placeIds);
+	
+	/**
+	 * Update the favorite places for a user.
+	 * 
+	 * @param userId
+	 * @param placeIds
+	 */
+	public void updateFavoritePlaces(String userId, List<String> placeIds);
+
+	/**
+	 * Get the favorite place ids for a user.
+	 * 
+	 * @param userId
+	 * @return placeIds
+	 */
+	public List<String> getFavoritePlaceIds(String userId);
+
+	/**
+	 * Get the favorite places for a user.
+	 * 
+	 * @param userId
+	 * @return set of places
+	 */
+	public List<Place> getFavoritePlaces(String userId);
+
+	/**
+	 * Gets photos based on place id
+	 * 
+	 * @param placeId
+	 * @return set of categories
+	 */
+	public Set<String> getPhotos(String placeId);
+	
+	/**
+	 * Gets types based on place id
+	 * 
+	 * @param placeId
+	 * @return set of types
+	 */
+
+	public Set<String> getTypes(String placeId);
+	
+	/**
+	 * Update the order of this user-place primary key. (Important function in Order Update)
+	 * If it exist, just update the order; if it doesn't exist, create the new key and add an order
+	 * @param userId
+	 * @param placeId
+	 * @param order
+	 * @return void
+	 */
+
+	public void updateOrder(String userId, String placeId, int order);
+	
+	/**
+	 * (debug function) check if there's any duplicate order after order update 
+	 * @param userId
+	 * @param List of placeId that has been updated
+	 * @param Update list of placeIds that has been updated
+	 * @return String, the problem placeId, which never showed up in updated list but got
+	 * 			the same order as one of the placeId in update list. If not found such 
+	 *          problem id, return null
+	 */
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public String checkOrder(String userId, List<String> placeIds);
+	
+	/**
+	 * Search items near a geolocation and a term (optional).
+	 * 
+	 * @param userId
+	 * @param placeName
+	 * 			(Nullable)
+	 * @param placeType
+	 *          (Nullable)
+	 * @return list of items
+	 */
+	public List<Place> searchPlaces(String placeName, String placeType);
+
+	/**
+	 * Save item into db.
+	 * 
+	 * @param item
+	 */
+	public void savePlace(Place place);
+
+	/**
+	 * Get full name of a user. (This is not needed for main course, just for demo
+	 * and extension).
+	 * 
+	 * @param userId
+	 * @return full name of the user
+	 */
+	public String getUsername(String userId);
+
+	/**
+	 * Return whether the credential is correct. (This is not needed for main
+	 * course, just for demo and extension)
+	 * 
+	 * @param userId
+	 * @param password
+	 * @return boolean
+	 */
+	public boolean verifyLogin(String userId, String password);
 
 }
--- src/db/mysql/MySQLConnection.java
+++ src/db/mysql/MySQLConnection.java
@@ -2,21 +2,236 @@ package db.mysql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import db.DBConnection;
+import db.DBConnectionFactory;
+import entity.Place;
+import external.GoogleMapsSearchPlaceAPI;
+import rpc.RpcHelper;
 
-public class MySQLConnection implements DBConnection{
+public class MySQLConnection implements DBConnection {
 
 	private Connection conn;
-	
+
 	public MySQLConnection() {
 		try {
-				Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
-				conn = DriverManager.getConnection(MySQLDBUtil.URL);
+			Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+			conn = DriverManager.getConnection(MySQLDBUtil.URL);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	@Override
+	public void close() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void setFavoritePlaces(String userId, List<String> placeIds) {
+		// TODO Auto-generated method stub
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return;
+		}
+
+		int largestOrder = 0;
+		try {							
+			String sql = "SELECT MAX(access_order) FROM favorites WHERE user_id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, userId);
+			ResultSet rs = ps.executeQuery();
+			
+			// if there is at least one favorite place for this user, get the order for further use
+			if (rs.next()) { 
+				largestOrder = rs.getInt(1);
+			} 
+
+			for (String placeId : placeIds) {
+				updateOrder(userId, placeId, ++largestOrder);
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void unsetFavoritePlaces(String userId, List<String> placeIds) {
+		// TODO Auto-generated method stub
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return;
+		}
+		
+		int largestOrder = 0;
+		List<String> placeList = new ArrayList<>();
+		try {
+			// delete rows first
+			String sql = "DELETE FROM favorites WHERE user_id = ? AND place_id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, userId);
+			for (String placeId : placeIds) {
+				ps.setString(2, placeId);
+				ps.execute();
+			}
+			
+			// reset order after delete
+			sql = "SELECT * FROM favorites WHERE user_id = ? ORDER BY access_order ASC";					
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, userId);
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				placeList.add(rs.getString("place_id"));
+			}
+			
+			for (String placeId : placeList) {
+				updateOrder(userId, placeId, ++largestOrder);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void updateFavoritePlaces(String userId, List<String> placeIds) {
+		// TODO Auto-generated method stub
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return;
+		}
+
+		int largestOrder = 0;
+		try {
+			for (String placeId : placeIds) {
+				updateOrder(userId, placeId, ++largestOrder);
+			}			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public List<String> getFavoritePlaceIds(String userId) {
+		// TODO Auto-generated method stub
+		// the sequence in the list relies on order!
+		return null;
+	}
+
+	@Override
+	public List<Place> getFavoritePlaces(String userId) {
+		// TODO Auto-generated method stub
+		// the sequence in the list relies on order!
+		return null;
+	}
+
+	@Override
+	public Set<String> getPhotos(String placeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<String> getTypes(String placeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateOrder(String userId, String placeId, int order) {
+		// TODO Auto-generated method stub
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return;
+		}
+
+		try {
+			String sql = "SELECT * FROM favorites WHERE user_id = ? AND place_id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, userId);
+			ps.setString(2, placeId);
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) { // if it is already in the table, update the order
+				try {
+					sql = "UPDATE favorites f " 
+							+ "SET f.access_order = ? " 
+							+ "WHERE f.user_id = ? AND f.place_id = ?";
+					PreparedStatement stmt = conn.prepareStatement(sql);
+					stmt.setInt(1, order);
+					stmt.setString(2, userId);
+					stmt.setString(3, placeId);
+					stmt.execute();
+					stmt.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else { // it it is not in the table yet, insert the column with new order
+				try {
+					sql = "INSERT IGNORE INTO favorites(user_id, place_id, access_order) VALUES (? ,? ,?)";
+					PreparedStatement stmt = conn.prepareStatement(sql);
+					stmt.setString(1, userId);
+					stmt.setString(2, placeId);
+					stmt.setInt(3, order);
+					stmt.execute();
+					stmt.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public String checkOrder(String userId, List<String> placeIds) {
+		// TODO Auto-generated method stub		
+		return null;
+	}
+
+	// deletable
+	@Override
+	public List<Place> searchPlaces(String placeName, String placeType) {
+		// TODO Auto-generated method stub
+		return new ArrayList<Place>();
+	}
+
+	@Override
+	public void savePlace(Place place) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public String getUsername(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean verifyLogin(String userId, String password) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
