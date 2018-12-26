76cdd5af0f8d26bd82faf497c191b0e139a8b78f
Eric <fupeng398517935@gmail.com>
2018-12-26 02:17:50 -0800

    structure clean up & small bugs fix

--- src/rpc/Login.java
+++ src/rpc/Login.java
@@ -2,9 +2,13 @@ package rpc;
 
 import java.io.IOException;
 
-import org.json.JSONObject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+import org.json.JSONObject;
 
 import db.DBConnection;
 import db.DBConnectionFactory;
--- src/rpc/Signup.java
+++ src/rpc/Signup.java
@@ -2,9 +2,14 @@ package rpc;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.json.JSONObject;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
 
 import db.DBConnection;
 import db.DBConnectionFactory;
