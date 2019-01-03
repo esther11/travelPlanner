afb6fd1f5816b14612e605f1ee0f4a864d747735
Eric <fupeng398517935@gmail.com>
2019-01-03 03:44:41 -0800

    signup location edit

--- WebContent/resource/scripts/header.js
+++ WebContent/resource/scripts/header.js
@@ -9,7 +9,7 @@
 	function initialize() {
 		var myplan = $("header-myplan");
 		var search = $("header-search");
-		var login = $("login/logout-link");
+		var login = $("login-logout-link");
 		var welcome = $("welcome-msg");
 		var signupBtn = $("Signup-link")
 
